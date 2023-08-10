5b8d68b4abe558cc44408a3710b64f3413836e23
Jesus Vasquez Medina <jesusvasquezm098@gmail.com>
2023-08-10 11:29:44 -0300

    Adicionar novas dependências no projeto; Mapear uma entidade JPA e criar uma interface Repository para ela; Utilizar os padrões DTO e Repository;

---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,33 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+
+    public Endereco(DadosEndereco endereco) {
+        this.logradouro = endereco.logradouro();
+        this.bairro = endereco.bairro();
+        this.cep = endereco.cep();
+        this.numero = endereco.numero();
+        this.complemento = endereco.complemento();
+        this.cidade = endereco.cidade();
+        this.uf = endereco.uf();
+    }
+}

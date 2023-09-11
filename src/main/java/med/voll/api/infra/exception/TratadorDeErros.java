17e4eba9cbff96b1e691b481f975bcbf09e900a9
Jesus Vasquez Medina <jesusvasquezm098@gmail.com>
2023-09-11 11:15:41 -0300

    Utilizar a biblioteca Auth0 java-jwt para realizar a validação dos tokens recebidos na API; Realizar o processo de autenticação da requisição, utilizando a classe SecurityContextHolder, do Spring; Liberar e restringir requisições, de acordo com a URL e o verbo do protocolo HTTP.

--- src/main/java/med/voll/api/infra/exception/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/exception/TratadorDeErros.java
@@ -1,11 +1,17 @@
 package med.voll.api.infra.exception;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.naming.AuthenticationException;
 
 @RestControllerAdvice
 public class TratadorDeErros {
@@ -19,6 +25,31 @@ public class TratadorDeErros {
     private ResponseEntity tratarErro400(MethodArgumentNotValidException exception){ //Erro 400 BAD REQUEST
    var error = exception.getFieldErrors();
         return ResponseEntity.badRequest().body(error.stream().map(DadoErroValidacao::new));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity tratarErro400(HttpMessageNotReadableException exception) {
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity tratarErroBadCredentials() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity tratarErroAuthentication() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falha na autenticação");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity tratarErroAcessoNegado() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Acesso negado");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity tratarErro500(Exception exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro: " +exception.getLocalizedMessage());
     }
 
     private record DadoErroValidacao(String field, String message){

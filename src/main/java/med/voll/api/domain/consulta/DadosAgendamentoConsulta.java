c232ec69e17bd7ed1be8a7a50d49a3bea331330c
Jesus Vasquez Medina <jesusvasquezm098@gmail.com>
2023-09-14 12:16:47 -0300

    Anotações JsonAlias e JsonFormat

--- src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
@@ -1,5 +1,7 @@
 package med.voll.api.domain.consulta;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
 import med.voll.api.domain.medico.Especialidade;
@@ -7,11 +9,14 @@ import med.voll.api.domain.medico.Especialidade;
 import java.time.LocalDateTime;
 
 public record DadosAgendamentoConsulta(
+        @JsonAlias("id_Medico")
         Long idMedico,
         @NotNull
+        @JsonAlias("id_Paciente")
         Long idPaciente,
         @NotNull
         @Future
+        @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
         LocalDateTime data,
         Especialidade especialidade) {
 }
